d48621042c417e1846217b626c20576cacc03742
carl joven <carl.j@anino.co>
2019-01-09 19:20:14 +0800

    Setup the splash scene

---
+++ Assets/Game/Scripts/SplashManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Tweeners;
+
+public class SplashManager : MonoBehaviour 
+{
+	[SerializeField] private Fader logoFader;
+	[SerializeField] private float transitionTime;
+
+	private bool preTransition = true;
+
+	void Start ()
+	{
+		logoFader.Play();
+		Invoke("StartTransition", transitionTime);
+	}
+
+	void StartTransition ()
+	{
+		SplashSceneManager.StartGame();
+	}
+}
+
+public class SplashSceneManager 
+{
+	public static void StartGame ()
+	{
+		SceneManager.LoadScene("UIScene", LoadSceneMode.Single);
+		SceneManager.LoadScene("GameScene", LoadSceneMode.Additive);
+	}
+}
